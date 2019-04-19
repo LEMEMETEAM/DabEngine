abeb66e67426896a355b0fe545b777aa93fedbac
LEMEME <bms110403@gmail.com>
2019-04-19 10:59:03 +0100

    Add files via upload

---
+++ DabEngineResources/src/DabEngineResources/DabEngineResources.java
@@ -0,0 +1,5 @@
+package DabEngineResources;
+public class DabEngineResources {
+	public static String version = "v0.0.1";
+	public static String info = "Jar that contains resources used by DabEngine.";
+}
