de47fff6e063233949ab13f1bfd3060a683f8c7c
LEMEME <bms110403@gmail.com>
2019-05-30 13:41:53 +0100

    Update TileMap.java

--- src/main/java/DabEngine/Graphics/TileMap.java
+++ src/main/java/DabEngine/Graphics/TileMap.java
@@ -11,6 +11,13 @@ import org.w3c.dom.Element;
 public class TileMap {
 
     private Document doc;
+    private class TileInfo {
+        public int width, height;
+        public int tileWidth, tileHeight;
+        public boolean infinite;
+    }
+    public TileInfo info = new TileInfo();
+    public lay
 
     public TileMap(File xmlFile){
         DocumentBuilderFactory fac = DocumentBuilderFactory.newInstance();
@@ -18,7 +25,25 @@ public class TileMap {
         doc = b.parse(xmlFile);
 
         Element mapRoot = doc.getDocumentElement();
+        info.width = Integer.valueOf(mapRoot.getAttribute("width"));
+        info.height = Integer.valueOf(mapRoot.getAttribute("height"));
+        info.tileWidth = Integer.valueOf(mapRoot.getAttribute("tileWidth"));
+        info.tileHeight = Integer.valueOf(mapRoot.getAttribute("tileHeight"));
+        boolean b;
+        int att = Integer.valueOf(mapRoot.getAttribute("infinite"));
+        if(att == 0){
+            b = false;
+        } else {
+            b = true;
+        }
+        info.infinite = b;
 
-        
+        NodeList layers = doc.getElementsByTag("layer");
+        for(int i = 0; i < layers.getLength(); i++){
+        }
     }
+}
+
+class Layers {
+    public ArrayList<MapObject> mObjs;
 }
