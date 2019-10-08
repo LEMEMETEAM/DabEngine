61fef585270940e3f277c39a8402745970d0fc69
LEMEME <bms110403@gmail.com>
2019-10-08 12:26:02 +0100

    Update ScriptManager.java

--- src/main/java/DabEngine/Script/ScriptManager.java
+++ src/main/java/DabEngine/Script/ScriptManager.java
@@ -1,27 +1,35 @@
 package DabEngine.Script;
 
 import org.luaj.vm2.*;
-import org.luaj.vm2.lib.OneArgFunction;
-import org.luaj.vm2.lib.ZeroArgFunction;
+import org.luaj.vm2.lib.LibFunction;
+import org.luaj.vm2.lib.LibFunction;
 import org.luaj.vm2.lib.jse.*;
 
 public class ScriptManager {
 
 	private static Globals globals = JsePlatform.standardGlobals();
 
-	public static void run(String script){
-		globals.loadfile(script).call();
+	public static void load(String script){
+		globals.loadfile(script);
 	}
 
-	public static void bind(String name, Object value){
-		globals.set(LuaValue.valueOf(name), CoerceJavaToLua.coerce(value));
+        public static void run(String script){
+                load(script).call();
+        }
+
+	public static void bindFunction(String name, LibFunction value){
+		globals.set(LuaValue.valueOf(name), value);
 	}
 
-	public static LuaValue getBindings(String name){
+        public static void bindValue(String name, Object value) {
+                globals.set(LuaValue.valueOf(name), CoerceJavaToLua.coerce(value));
+        }
+
+	public static LuaValue get(String name){
 		return globals.get(LuaValue.valueOf(name));
 	}
 
-	public static Varargs invokeMethod(String name, Object... args){
+	public static Varargs invokeFunction(String name, Object... args){
 		if(args != null && args.length > 0) {
 			LuaValue[] values = new LuaValue[args.length];
 			for(int i = 0; i < args.length; i++){
